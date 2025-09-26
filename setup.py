0425c4ea945514247610334b6b13e814919d8bc4
Grzegorz Malczyk <44407007+grzemal@users.noreply.github.com>
2025-09-26 17:22:38 +0200

    Update warp-lang dependency to version 1.0.0
    Signed-off-by: Grzegorz Malczyk <44407007+grzemal@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         "numpy",
         "torch",
         "pytorch3d",
-        "warp-lang==1.0.0b5",
+        "warp-lang==1.0.0",
         "trimesh",
         "urdfpy",
         "numpy==1.23",
